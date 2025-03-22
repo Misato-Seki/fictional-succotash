698d6611104e3148243655ca3fd5c4ff373cbd0e
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-03-22 17:33:50 +0200

    fix: solve type error

--- app/api/traffic/route.ts
+++ app/api/traffic/route.ts
@@ -1,5 +1,19 @@
 import { NextResponse } from "next/server";
 
+interface Location {
+    type: "Point";
+    coordinates: [number, number]; // [経度, 緯度]
+  }
+
+interface TrainData {
+    trainNumber: number; // 列車番号
+    departureDate: string; // 出発日（ISO 8601形式）
+    timestamp: string; // タイムスタンプ（ISO 8601形式）
+    location: Location; // 位置情報
+    speed: number; // 速度（km/h）
+    accuracy: number; // 位置の精度
+  }
+
 export async function GET() {
     try {
         const response = await fetch('https://rata.digitraffic.fi/api/v1/train-locations/latest');
@@ -10,7 +24,7 @@ export async function GET() {
 
         const data = await response.json();
 
-        const filteredData = data.map((item: any) => ({
+        const filteredData = data.map((item: TrainData) => ({
             trainNumber: item.trainNumber,
             location: item.location.coordinates,
         }))
